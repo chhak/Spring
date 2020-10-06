66b024d5fc17a5f1c831fab93a0ad06fde3540b7
aneklfae <anfekl@nael.com>
2020-10-06 09:32:14 +0900

    2020/10/06 회원가입 약관 구현

--- kmarket/src/main/java/kr/co/kmarket/controller/MemberController.java
+++ kmarket/src/main/java/kr/co/kmarket/controller/MemberController.java
@@ -1,12 +1,22 @@
 package kr.co.kmarket.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kr.co.kmarket.persistance.TermsRepo;
+import kr.co.kmarket.vo.TermsVo;
+
 @Controller
 public class MemberController {
-
+	
+	@Autowired
+	private TermsRepo repo;
+	
+	
 	@GetMapping("/member/login")
 	public String login() {
 		return "/member/login";
@@ -20,7 +30,10 @@ public class MemberController {
 	@GetMapping("/member/signup")
 	public String signup(String type, Model model) {
 		
+		TermsVo vo = repo.findById(0).get();
+		
 		model.addAttribute("type", type);
+		model.addAttribute(vo);
 		
 		return "/member/signup";
 	}
